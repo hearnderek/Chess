1a8883d7407017f027c01916d518cd454f6696b5
Derek Hearn <hearnderek@yahoo.com>
2020-07-26 01:00:33 +0900

    pylint

--- Chess.py
+++ Chess.py
@@ -1,80 +1,81 @@
+"""
+Hello Doc String
+"""
+
 import os
 clear = lambda: os.system('cls')
 
-def add_c(c,i):
-    return chr(ord(c) + i)
+def add_c(char, i):
+    """ ("A", 1) -> "B" """
+    return chr(ord(char) + i)
 
-def add_pos(pos,col,row):
+def add_pos(pos, col, row):
+    """ (E6, 1, -2) -> F4 """
     return add_c(pos[0], col) + add_c(pos[1], row)
 
 def direction_gen(pos, direction, board, team):
+    """ raytracing for possible moves """
     next_pos = add_pos(pos, direction[0], direction[1])
-    piece_at = board.piece_hash.get(next_pos, None)
     if can_go(next_pos, board, team):
         yield next_pos
         if next_pos not in board.piece_hash:
             yield from direction_gen(next_pos, direction, board, team)
 
-def around_gen(pos, board, team):
-    xs = [
-        (-1,1) ,(0,1) ,(1,1),
-        (-1,0)        ,(1,0),
-        (-1,-1),(0,-1),(1,-1)]
-    for x in xs:
-        print(x)
-        if can_go(pos, board, team):
-            yield add_pos(pos, x[0], x[1])
-
 def can_go(pos, board, team):
-    if pos not in board.wb:
+    """ Can I move there? Killing the other team is okay. """
+    if pos not in board.squares:
         return False
     piece_at = board.piece_hash.get(pos, None)
     if not piece_at or piece_at.team != team:
-            # take enemy piece
-            return True
+        # take enemy piece
+        return True
+    return False
 
 def around_dir_gen(pos, board, team):
-    xs = [
-        (-1,1) ,(0,1) ,(1,1),
-        (-1,0)        ,(1,0),
-        (-1,-1),(0,-1),(1,-1)]
-    for x in xs:
+    """ Generate the moves for the king """
+    directions = [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1)]
+    for direction in directions:
+        next_pos = add_pos(pos, direction[0], direction[1])
         if can_go(next_pos, board, team):
-            yield add_pos(pos, x[0], x[1])
+            yield next_pos
 
 def knight_gen(pos, board, team):
-    xs = [
-            (-1, 2)      ,(1,2),
-    (-2,1)                      ,(2,1),
-                
-    (-2,-1)                      ,(2,-1),
-            (-1,-2)      ,(1,-2)]
-    for x in xs:
-        next_pos = add_pos(pos, x[0], x[1])
+    """ Generate the moves for the knight """
+    directions = [(-1, 2), (1, 2), (-2, 1), (2, 1), (-2, -1), (2, -1), (-1, -2), (1, -2)]
+    for direction in directions:
+        next_pos = add_pos(pos, direction[0], direction[1])
         if can_go(next_pos, board, team):
-            yield next_pos 
+            yield next_pos
 
 class Piece():
+    """ Base class for all game pieces """
     def __init__(self):
         self.icon = "X"
         self.move_gens = list()
         self.team = 0
 
     def valid_moves(self, pos):
+        """
+        Using underlying self.move_gen generate all possible moves
+
+        move_gen is populated through the call to init_move_gens
+        """
         for move_gen in self.move_gens:
-            mg = move_gen(pos)
             yield from move_gen(pos)
 
     def set_team(self):
+        """ Flips the team bit """
         self.icon = self.icon.swapcase()
         # swaps between 0,1
         self.team = (self.team + 1) & 1
         return self
 
     def init_move_gens(self, board):
-        raise Exception("init_move_gens Not implemented")
+        """ All inheriting classes should implement this to populate self.move_gen """
+        raise Exception(str(type(self)) + ": init_move_gens Not implemented")
 
 class King(Piece):
+    """ The King """
     def __init__(self):
         Piece.__init__(self)
         self.icon = "K"
@@ -84,24 +85,26 @@ class King(Piece):
         self.move_gens.append(gen)
 
 class Queen(Piece):
+    """ The Queen """
     def __init__(self):
         Piece.__init__(self)
         self.icon = "Q"
 
     def init_move_gens(self, board):
-        up = lambda pos: direction_gen(pos, (0,1), board, self.team)
-        down = lambda pos: direction_gen(pos, (0,-1), board, self.team)
-        left = lambda pos: direction_gen(pos, (1,0), board, self.team)
-        right = lambda pos: direction_gen(pos, (-1,0), board, self.team)
-        
-        upleft = lambda pos: direction_gen(pos, (1,1), board, self.team)
-        upright = lambda pos: direction_gen(pos, (-1,1), board, self.team)
-        downleft = lambda pos: direction_gen(pos, (1,-1), board, self.team)
-        downright = lambda pos: direction_gen(pos, (-1,-1), board, self.team)
+        up = lambda pos: direction_gen(pos, (0, 1), board, self.team)
+        down = lambda pos: direction_gen(pos, (0, -1), board, self.team)
+        left = lambda pos: direction_gen(pos, (1, 0), board, self.team)
+        right = lambda pos: direction_gen(pos, (-1, 0), board, self.team)
+
+        upleft = lambda pos: direction_gen(pos, (1, 1), board, self.team)
+        upright = lambda pos: direction_gen(pos, (-1, 1), board, self.team)
+        downleft = lambda pos: direction_gen(pos, (1, -1), board, self.team)
+        downright = lambda pos: direction_gen(pos, (-1, -1), board, self.team)
         for gen in [up, down, left, right, upleft, upright, downleft, downright]:
             self.move_gens.append(gen)
 
 class Pawn(Piece):
+    """ The Pawn """
     def __init__(self):
         Piece.__init__(self)
         self.icon = "P"
@@ -113,13 +116,14 @@ class Pawn(Piece):
             self.move_gens.append(gen)
 
     def move_generator(self, pos, board):
-        direction = (self.team & 1) - (self.team+1 & 1) 
-        
-        attack = [(-1,direction), (1,direction)]
+        """ Strangly the pawn is the move conplicated mover """
+        direction = (self.team & 1) - (self.team + 1 & 1)
+
+        attack = [(-1, direction), (1, direction)]
         row = int(pos[1])
-        movement = [(0,direction)]
-        if row == 7 or row == 2:
-            movement.append((0,direction*2))
+        movement = [(0, direction)]
+        if row in (2, 7):
+            movement.append((0, direction * 2))
 
         for move in movement:
             next_pos = add_pos(pos, move[0], move[1])
@@ -132,32 +136,35 @@ class Pawn(Piece):
                 yield next_pos
 
 class Rook(Piece):
+    """ The Rook """
     def __init__(self):
         Piece.__init__(self)
         self.icon = "R"
 
     def init_move_gens(self, board):
-        up = lambda pos: direction_gen(pos, (0,1), board, self.team)
-        down = lambda pos: direction_gen(pos, (0,-1), board, self.team)
-        left = lambda pos: direction_gen(pos, (1,0), board, self.team)
-        right = lambda pos: direction_gen(pos, (-1,0), board, self.team)
+        up = lambda pos: direction_gen(pos, (0, 1), board, self.team)
+        down = lambda pos: direction_gen(pos, (0, -1), board, self.team)
+        left = lambda pos: direction_gen(pos, (1, 0), board, self.team)
+        right = lambda pos: direction_gen(pos, (-1, 0), board, self.team)
         for gen in [up, down, left, right]:
             self.move_gens.append(gen)
 
 class Bishop(Piece):
+    """ The Bishop """
     def __init__(self):
         Piece.__init__(self)
         self.icon = "B"
 
     def init_move_gens(self, board):
-        upleft = lambda pos: direction_gen(pos, (1,1), board, self.team)
-        upright = lambda pos: direction_gen(pos, (-1,1), board, self.team)
-        downleft = lambda pos: direction_gen(pos, (1,-1), board, self.team)
-        downright = lambda pos: direction_gen(pos, (-1,-1), board, self.team)
+        upleft = lambda pos: direction_gen(pos, (1, 1), board, self.team)
+        upright = lambda pos: direction_gen(pos, (-1, 1), board, self.team)
+        downleft = lambda pos: direction_gen(pos, (1, -1), board, self.team)
+        downright = lambda pos: direction_gen(pos, (-1, -1), board, self.team)
         for gen in [upleft, upright, downleft, downright]:
             self.move_gens.append(gen)
 
 class Knight(Piece):
+    """ The Knight """
     def __init__(self):
         Piece.__init__(self)
         self.icon = "H"
@@ -167,45 +174,44 @@ class Knight(Piece):
         self.move_gens.append(gen)
 
 class Board():
+    """ The Knight """
     def __init__(self):
-        self.rows = list(range(8,0,-1))
+        self.rows = list(range(8, 0, -1))
         self.cols = list("ABCDEFGH")
         self.piece_hash = dict()
 
         # true/false for showing white or black for squares
         # because we're adding the chr and int we get the desired pattern via odd/even test
         all_keys = [col+str(row) for row in self.rows for col in self.cols]
-        self.wb = dict([(k,(ord(k[0])+int(k[1])+1)%2==0) for i,k in enumerate(all_keys)])
+        enumerated_keys = enumerate(all_keys)
+        self.squares = dict([(k, (ord(k[0]) + int(k[1]) + 1) % 2 == 0) for i, k in enumerated_keys])
 
     def box(self, key):
-        if self.wb[key]:
+        """ String to draw the board, with room for a piece """
+        if self.squares[key]:
             # white
             return "[{}]"
-        else:
-            # black
-            return ":{}:"
-
-    def occupied(self, loc):
-        return key in self.piece_hash
+        return ":{}:"
 
     def move(self, start, end):
+        """ Move from here to there """
         self.piece_hash[end] = self.piece_hash[start]
         del self.piece_hash[start]
 
     def icon_at(self, key):
+        """ get icon to draw at keyed location """
         piece = self.piece_hash.get(key, None)
-        return (piece.icon if piece else " ")
-    
+        return piece.icon if piece else " "
+
     def draw(self):
+        """ Draw the populated board """
         for row in self.rows:
             keys = [self.box(col+str(row)).format(self.icon_at(col+str(row))) for col in self.cols]
             print("".join(keys))
-            
-    def print(self):
-        for row in self.rows:
-            print(" ".join([col+str(row) for col in self.cols]))
+
 
 def standard_start(board):
+    """ Populate a black board with pieces """
     board.piece_hash["A1"] = Rook().set_team()
     board.piece_hash["B1"] = Knight().set_team()
     board.piece_hash["C1"] = Bishop().set_team()
@@ -245,22 +251,23 @@ def standard_start(board):
     for piece in board.piece_hash.values():
         piece.init_move_gens(board)
 
-def is_valid(key, board, team):
+def try_user_movement(key, board, team):
+    """ Is the User Input string valid? """
     if not key:
         print("Null Input")
         return False
 
-    s = key.upper().split(",")
-    if len(s) != 2:
-        print("Bad format", s)
+    splits = key.upper().split(",")
+    if len(splits) != 2:
+        print("Bad format", splits)
         return False
 
-    start = s[0]
-    end = s[1]
+    start = splits[0]
+    end = splits[1]
     if start not in board.piece_hash:
         print("can't find", start)
         return False
-    
+
     piece = board.piece_hash[start]
     if piece.team != team:
         print("Wrong team.")
@@ -270,11 +277,12 @@ def is_valid(key, board, team):
     if end not in possibles:
         print("can't move to", end)
         return False
-    
+
     board.move(start, end)
     return True
 
 def main():
+    """ Program Entry Point """
     board = Board()
     standard_start(board)
 
@@ -290,69 +298,10 @@ def main():
         while True:
             userin = input()
             print()
-            if userin == "exit" or is_valid(userin, board, team):
+            if userin == "exit" or try_user_movement(userin, board, team):
                 team = (team + 1) & 1
                 break
-            else: 
-                print("Invalid.")
-
-def test():
-    board = Board()
-    board.print()
-
-    print()
-    standard_start(board)
-    board.draw()
-
-    knights = [(k,v) for (k,v) in board.piece_hash.items() if isinstance(v, Knight)]
-
-    locs = set()
-    for (key, knight) in knights:
-        knight.init_move_gens(board)
-        for loc in list(knight.valid_moves(key)):
-            board.piece_hash[loc] = Piece()
-            locs.add(loc)
-    print()
-    print("All possible moves for knights")
-    board.draw()
-    for loc in locs:
-        del board.piece_hash[loc]
-
-    queens = [(k,v) for (k,v) in board.piece_hash.items() if isinstance(v, Queen)]
-
-    locs = set()
-    for (key, queen) in queens:
-        queen.init_move_gens(board)
-        for loc in list(queen.valid_moves(key)):
-            board.piece_hash[loc] = Piece()
-            locs.add(loc)
-    
-    print()
-    print("All possible moves for queens")
-    board.draw()
-
-    for loc in locs:
-        del board.piece_hash[loc]
-
-
-
-
-    # q = Queen()
-    # k = King()
-    # p = Pawn()
-    # board.piece_hash["A8"] = q
-    # board.piece_hash["B2"] = k
-    # board.piece_hash["E2"] = p
-    # board.draw()
-    # print()
-
-    # board.move("B2", list(k.valid_moves("B2"))[2])
-    # print(board.piece_hash)
-
-    # board.draw()
-
-    
-    
+            print("Invalid.")
 
 if __name__ == "__main__":
     print("Chess\n")
